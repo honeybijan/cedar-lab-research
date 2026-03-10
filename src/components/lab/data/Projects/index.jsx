0f6bfa4346e1204ad77b62e06c6c22f70152e1e4
honeybijan <bijanmazaheri@gmail.com>
2026-03-09 22:58:16 -0400

    masking -> masked

--- src/components/lab/data/Projects/index.jsx
+++ src/components/lab/data/Projects/index.jsx
@@ -19,7 +19,7 @@ export const PROJECTS = [
     order: 2,
     description: "We often need to understand how specific information influences decision-making. Because data is frequently entangled, determining whether a decision relies on a direct attribute or a correlated feature is notoriously difficult. In fact, this is an **information-theoretically** hard problem due to the complexities of testing for conditional independence. We are actively researching this challenge, with applications in algorithmic fairness and the detection of covert decision-making.",
     paperTitles: [
-      "Masking Unfairness: Hiding Causality in Zero ATE",
+      "Masked Unfairness: Hiding Causality in Zero ATE",
     ],
   },
   {
